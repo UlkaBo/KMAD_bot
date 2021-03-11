dcc6954a59267b84c2453d746b8d8acb1949e130
UlkaBo <ul.bozhko@gmail.com>
2021-03-11 11:58:38 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -9,7 +9,9 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler,
 from telegram import Update, Bot, ReplyKeyboardRemove
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 import datetime
-TOKEN = '1638396072:AAG5P6RseKWvZAY7qZIUelkjAAlOJBMWrk0'
+# 1638396072:AAG5P6RseKWvZAY7qZIUelkjAAlOJBMWrk0
+TOKEN = '1638396072:AAGbvBq4UMpmXFBqapwD0V5sxHfxdSPgveI'
+
 
 link = 'https://ulkabo.github.io/content-for-telegtam-bot/data/'
 
