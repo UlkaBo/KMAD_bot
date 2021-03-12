16185797de8250e7d6f51357f0dd57d81137f8bc
UlkaBo <ul.bozhko@gmail.com>
2021-03-12 18:20:08 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -466,10 +466,8 @@ def main():
     dp.add_handler(CallbackQueryHandler(posylannya, pattern="posylannya"))
     dp.add_handler(CallbackQueryHandler(kilkistMists, pattern="kilkistMists"))
 
-    # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, echo))
-
     # log all errors
+
     dp.add_error_handler(error)
 
     # Start the Bot
