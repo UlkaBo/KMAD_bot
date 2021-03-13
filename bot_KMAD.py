0dfbe26e157283ab0bdc6e4c286bd9c9b1de3010
UlkaBo <ul.bozhko@gmail.com>
2021-03-13 16:13:24 +0200

    Update bot_KMAD.py

--- bot_KMAD.py
+++ bot_KMAD.py
@@ -466,8 +466,6 @@ def main():
     dp.add_handler(CallbackQueryHandler(posylannya, pattern="posylannya"))
     dp.add_handler(CallbackQueryHandler(kilkistMists, pattern="kilkistMists"))
 
-    # log all errors
-
     dp.add_error_handler(error)
 
     # Start the Bot
