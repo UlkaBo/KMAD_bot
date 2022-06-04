8aa0d76a13d53852d7c70b2b8d746477fb49ceb3
UlkaBo <ul.bozhko@gmail.com>
2022-06-04 18:30:45 +0300

    delete using boto

--- config_.py
+++ config_.py
@@ -1,3 +1,4 @@
 import os
-from boto.s3.connection import S3Connection
-TOKEN = S3Connection(os.environ['TOKEN'])
+#from boto.s3.connection import S3Connection
+#TOKEN = S3Connection(os.environ['TOKEN'])
+TOKEN = os.environ['TOKEN']
